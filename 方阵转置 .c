b23de7910c51b656656d3913721e041d0086ec9d
HCYETY <70583417+HCYETY@users.noreply.github.com>
2020-11-24 21:30:37 +0800

    2020.11.24

--- 方阵转置 .c
+++ 方阵转置 .c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 int main(void)
-  {int i, j, n, temp;
-   int a[6][6];
-   printf("Enter n: ");
-   scanf("%d", &n);
-   for(i=0; i<3; i++ ) {
-   	for(j=0; j<2; j++ ) {
-   		scanf ("%d", &a[i][j]) ;
+  {int i, j,  temp;
+   int a[3][3];
+   for(i=0; i<3; i++) {
+   	for(j=0; j<3; j++) {
+   		scanf("%d", &a[i][j]) ;
   		}
 	}
-   for(i=0; i<n; i++) {
-		for(j=0; j<n; j++) {
+   for(i=0; i<3; i++) {
+		for(j=0; j<3; j++) {
 			if(i<=j) {  /* 只遍历上三角阵 */
          	temp = a[i][j];
 		   	a[i][j] = a[j][i];
 				a[j][i] = temp;
-				printf ( "%4d", a[i][j] );
-           printf ( "\n" );
-
          }
 		}
 	}
-   printf("%d\n", a[i][j]);
+	for(i=0; i<3; i++) {
+      for(j=0; j<3; j++) {
+         printf("%4d", a[i][j]);
+      }
+         printf("\n");
+   }
    return 0;
   }
 
