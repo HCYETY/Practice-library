f096ed272ab93ccaef7db29abf64ad954ea41061
HCYETY <70583417+HCYETY@users.noreply.github.com>
2020-11-29 22:05:38 +0800

    2020.11.29

---
+++ A+B（1）.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+  {double A, B, sum;
+   A <= fabs ( 10 * 15 );
+   B <= fabs ( 10 * 15 );
+   scanf("%lf %lf",&A,&B);
+   sum = A + B;
+   if ( sum - (int)sum == 0) {
+       printf("%.lf",sum);
+   } else {
+        printf("%.2lf",sum);
+   }
+   return 0;
+  }
